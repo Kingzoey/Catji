77e6fd0a01189a0ae5e652450168fde99fb28fa4
JYJ <1205748010@qq.com>
2020-07-30 18:57:21 +0800

    dev

---
+++ src/components/Middle.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="hot-part">
+        <header class="hot-header">
+            <span class="name">最热</span>
+        </header>
+        <div class="hot-list-top">
+            <div class="top-left">
+               <div class="big-item">
+                    <a class="cover" href="#" target="_blank">
+                        <img :src="test1" width="200" alt="最热视频1"/>
+                    </a>
+                     <br>
+                    <a title="最热视频1">最热视频1</a>
+                </div>
+            </div>
+            <div class="top-right">
+               <div class="top-right-line">
+                    <div class="normal-item">
+                        <a class="cover" href="#" target="_blank">
+                            <img :src="test1" width="150" alt="最热视频2"/>
+                        </a>
+                        <br>
+                        <a title="最热视频2">最热视频2</a>
+                    </div>
+                    <div class="normal-item">
+                        <a class="cover" href="#" target="_blank">
+                            <img :src="test1" width="150" alt="最热视频2"/>
+                        </a>
+                            <br>
+                        <a title="最热视频2">最热视频2</a>
+                    </div>
+                    <div class="normal-item">
+                        <a class="cover" href="#" target="_blank">
+                            <img :src="test1" width="150" alt="最热视频2"/>
+                        </a>
+                            <br>
+                        <a title="最热视频2">最热视频2</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import test1 from "F:/Catji/Catji/src/assets/test.png";
+export default {
+    name: "Middle",
+    data() {
+        return {
+            test1: test1,
+        };
+  }
+}
+</script>
+
+<style>
+.top-left{
+    margin-top:50px;
+    float:left;
+    margin-left:300px;
+}
+.top-right{
+    float:right;
+    margin-right: 400px;
+}
+.normal-item{
+    float:left;
+    border:200px;
+    margin-left: 50px;
+}
+.top-right-line{
+    margin-top:20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Top style="background-color: #f0f0f0;"></Top>
+    <Middle></Middle>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Top from "@/components/Top.vue";
+import Middle from "@/components/Middle.vue";
 export default {
   name: "Home",
   components: {
     Top,
+    Middle,
   },
 };
 </script>
