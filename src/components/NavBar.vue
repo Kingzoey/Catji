1e09b8b6a0489b502f20587261ec10c712384f50
wujinmengchen <2742462891@qq.com>
2020-08-01 13:00:47 +0800

    dao hang lan

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,13 +7,14 @@
       <router-link to="/login">Login</router-link>|
       <router-link to="/register">Register</router-link>|
       <router-link to="/space">Space</router-link>|
-      <router-link to="/search">Search</router-link>
-    </div>
-    
+      <router-link to="/search">Search</router-link>|
     <!-- 左侧导航栏 -->
-    <div>
-
-    </div>
+	<router-link to="/mine">Mine</router-link>|
+	<router-link to="/message">Message</router-link>|
+	<router-link to="/blog">Blog</router-link>|
+	<router-link to="/favorite">Favorite</router-link>|
+	<router-link to="/release">Release</router-link>
+	</div>
   </nav>
 </template>
 
@@ -24,7 +25,19 @@ export default {
 </script>
 
 <style scoped>
-nav {
-  background-color: black;
+nav{
+	float: inherit;
+	width:1700px;
+	height:140px;
+	background-image:url(../assets/Nav.png);
+	margin: 10px 0 0 0;
+	padding:5px 400px;
+}
+nav div router-link:hover{
+	color: white;
+}
+.sidebar div{
+  height: 40px;
+  line-height: 10px;
 }
 </style>
