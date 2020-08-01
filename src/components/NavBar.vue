741228957bac5abe8c454ebd8993a72a461d31de
wujinmengchen <2742462891@qq.com>
2020-08-01 15:33:07 +0800

    wan shan dao hang lan

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,12 @@
 <template>
-  <nav>
-    <!-- 左侧导航栏 -->
-    <div>
+	<!--
+      <div>
       <router-link to="/">Home</router-link>|
       <router-link to="/home_0">Home_0</router-link>|
       <router-link to="/login">Login</router-link>|
       <router-link to="/register">Register</router-link>|
       <router-link to="/space">Space</router-link>|
       <router-link to="/search">Search</router-link>|
-    <!-- 左侧导航栏 -->
 	<router-link to="/mine">Mine</router-link>|
 	<router-link to="/message">Message</router-link>|
 	<router-link to="/blog">Blog</router-link>|
@@ -16,6 +14,29 @@
 	<router-link to="/release">Release</router-link>
 	</div>
   </nav>
+  -->
+  <div class="top">
+			<div class="a">
+				<div class="b">
+
+					<ul style="float: left;">
+						<li><router-link to="/">首页</router-link></li>
+						<li><router-link to="/home_0">首页_0</router-link></li>
+						<li><router-link to="/login">登录</router-link></li>
+						<li><router-link to="/register">注册</router-link></li>
+						<li><router-link to="/search">搜索</router-link></li>
+					</ul>
+
+					<ul style="float: right;">
+						<li><router-link to="/mine">用户</router-link></li>
+						<li><router-link to="/message">消息</router-link></li>
+						<li><router-link to="/blog">动态</router-link></li>
+						<li><router-link to="/favorite">收藏</router-link></li>
+						<li class="tougao"><router-link to="/release">投稿</router-link></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 </template>
 
 <script>
@@ -25,6 +46,7 @@ export default {
 </script>
 
 <style scoped>
+/*
 nav{
 	float: inherit;
 	width:1700px;
@@ -40,4 +62,47 @@ nav div router-link:hover{
   height: 40px;
   line-height: 10px;
 }
+*/
+*{margin: 0; padding: 0;} 
+.top {
+	width: 100%;
+	height: 100px;
+	background: url(../assets/Nav.png);}
+
+.top .a {
+	width: 100%;
+	height: 38px;
+	background: rgba(255, 255, 255, 0.5);}
+	
+.top .a .b {
+	width: 1000px;
+	height: 38px;
+	margin: 0 auto;}
+
+.top .a .b ul li{
+	width: 50px;
+	height: 38px;
+	float: left;
+	list-style: none;
+	line-height: 38px;
+	text-align: center;
+	font-size: 16px;
+	font-family: 微软雅黑;
+	font-weight: 100px;}
+
+.top .a .b ul li:hover{
+	background-color: aqua;}
+
+.top .a .b ul li a {
+	text-decoration: none;
+	width: 50px;
+	height: 38px;
+	display: block;
+	color: aliceblue;}
+
+.top .a .b ul li.tougao{
+	height: 55px;
+	background: pink;
+	border-radius:0 0 15px 15px;}
+
 </style>
