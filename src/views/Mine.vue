b73862eba764ecf30740b86d87b0ce0629680327
Frezcirno <2277861660@qq.com>
2020-08-01 16:03:53 +0800

    修复了build-gh失败的问题 添加了模板内容

---
+++ src/views/Mine.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Mine",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+};
+</script>
+
+<style scoped>
+</style>
