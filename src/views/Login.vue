dbd174558a5aa1c820e9edcbd44e049e53f1ae8a
Kingzoey <729650614@qq.com>
2020-08-02 22:02:24 +0800

    Update Login and Register vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,16 @@
 <template>
-  <div></div>
+  <div class="login">
+    <Loginn style="background-color: #f0f0f0;"></Loginn>
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import Loginn from "@/components/Loginn.vue";
 export default {
   name: "Login",
+  components: {
+    Login,
+  },
 };
 </script>
-
-<style scoped>
-</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,12 +1,16 @@
 <template>
-  <div></div>
+  <div class="register">
+    <Registerr style="background-color: #f0f0f0;"></Registerr>
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import Registerr from "@/components/Registerr.vue";
 export default {
   name: "Register",
+  components: {
+    Register,
+  },
 };
 </script>
-
-<style scoped>
-</style>
