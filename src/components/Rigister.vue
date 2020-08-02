a8dc49fb50ddbc2f3d94ee261b6bee56c877c2b7
JYJ <1205748010@qq.com>
2020-08-02 22:22:14 +0800

    dev

---
+++ src/components/Rigister.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="rigister">
+         <div class="header">
+            <div class="logo">
+                <a href="#">
+                    <img src="" alt="" srcset="" width="150">
+                </a>
+            </div>
+        </div>
+        
+        <div class="mainbody">
+            <img src="" width="400" height="300">
+            <div class="info">
+                <div class="log_list">
+                    <ul class="input">
+                        <li class="user">
+                        <input type="text" placeholder="用户名/邮箱/手机号">
+                        </li>
+                        <li class="user">
+                            <input type="text" placeholder="密码">
+                        </li>
+                        <li class="remember_password">
+                        <input type="checkbox">
+                            <label for="">记住密码</label>
+                            <a href="#">忘记密码</a>
+                        </li>
+                        <li class="log_button">
+                            <button>登录</button>
+                        </li>
+                        <li class="log_choose">
+                            <a href="#">立即注册</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="rigister">
+    <Top style="background-color: #f0f0f0;"></Top>
+    <rigister></rigister>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import rigister from "@components/Rigister.Vue"
+export default {
+  name: "Home",
+  components: {
+    rigister,
+  },
+};
+</script>
