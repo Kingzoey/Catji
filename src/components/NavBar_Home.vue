d32f3487007179909c91e771b9c8b748381f03d6
Kingzoey <729650614@qq.com>
2020-08-03 14:39:12 +0800

    Fix all NavBar_Home.

--- src/components/NavBar_Home.vue
+++ src/components/NavBar_Home.vue
@@ -3,40 +3,24 @@
     <div class="header">
       <div class="nav">
         <ul>
-          <li>
-            <router-link to="/">首页</router-link>
-          </li>
-          <li>
-            <router-link to="/login">登录</router-link>
-          </li>
-          <li>
-            <router-link to="/register">注册</router-link>
-          </li>
-          <li>
-            <router-link to="/search">
+          <li><router-link to="/">首页</router-link></li>
+          <li><router-link to="/login">登录</router-link></li>
+          <li><router-link to="/register">注册</router-link></li>
+          <li><router-link to="/search">
               <font-awesome-icon :icon="['fas', 'search']" />
-            </router-link>
-          </li>
+            </router-link></li>
         </ul>
-
-        <ul style="float: right;">
-          <li>
-            <router-link to="/mine">
+      </div>
+      <div class="nav">
+        <ul>
+          <li><router-link to="/space">
               <font-awesome-icon :icon="['fas', 'user']" />
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/message">消息</router-link>
-          </li>
-          <li>
-            <router-link to="/blog">动态</router-link>
-          </li>
-          <li>
-            <router-link to="/favorite">收藏</router-link>
-          </li>
-          <li class="tougao">
-            <router-link to="/release">投稿</router-link>
-          </li>
+            </router-link></li>
+          <li><router-link to="/message">消息</router-link></li>
+          <li><router-link to="/blog">动态</router-link></li>
+          <li><router-link to="/favorite">收藏</router-link></li>
+          <li class="upload">
+            <router-link to="/upload">投稿</router-link></li>
         </ul>
       </div>
     </div>
@@ -51,48 +35,54 @@ export default {
 
 <style scoped>
 .header-box {
-  width: 100%;
-  height: 100px;
-  background: url(../assets/Nav.png);
+  background: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
 }
 
 .header {
-  width: 100%;
-  height: 38px;
-  background: rgba(255, 255, 255, 0);
-  position: relative;
-  z-index: 999;
-}
-
-.nav {
   width: 1000px;
-  height: 38px;
   margin: 0 auto;
+  line-height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
-.nav ul li {
-  width: 50px;
-  height: 38px;
-  float: left;
-  list-style: none;
-  line-height: 38px;
-  text-align: center;
-  font-size: 16px;
-  font-family: 微软雅黑;
-  font-weight: 100px;
-}
-
-.nav ul li header {
-  text-decoration: none;
-  width: 50px;
-  height: 38px;
-  display: block;
-  color: aliceblue;
-}
-
-.nav ul li.tougao {
+.header-box,
+.header,
+.nav-left ul,
+.nav-right ul {
   height: 55px;
-  background: pink;
+}
+
+.nav ul {
+  display: flex;
+  align-items: center;
+}
+
+.nav li {
+  width: 50px;
+  text-align: center;
+  font-size: 12pt;
+  transition: all 0.3s;
+}
+
+.nav li a {
+  color: #222;
+  display: block;
+}
+
+.nav li a:hover {
+  color: #fb7299;
+}
+
+.upload {
+  line-height: 75px;
+  background: #fb7299;
   border-radius: 0 0 15px 15px;
+}
+
+.upload a {
+  color: #fff !important;
 }
 </style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
+import NavBar from "@/components/NavBar_Home.vue";
 export default {
   name: "Search",
   components: {
