480ad74ebe3c1325fcb96a2a5b7a45181788355b
JYJ <1205748010@qq.com>
2020-08-07 14:12:54 +0800

    dev

---
+++ src/components/UploadFile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mainBody">
+       <div class="head">
+           <h2>上传文件</h2>
+           <span class="warning">请勿上传含有政治因素、色情挑逗、血腥暴力等违法因素<br>的视频内容</span>
+       </div>
+       <div class="File">
+           <a class="UploadIco" href="#" target="_blank">
+               <img src= "upload" height="50" width="20"> 
+            </a>
+           
+       </div>
+    </div>
+</template>
+
+<script>
+import upload from "@/assets/upload.png";
+export default {
+    name: "Middle",
+    data() {
+        return {
+            upload:upload,
+        };
+  }
+}
+</script>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <NavBar />
+    <UploadFile />
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar_Home.vue";
+import UploadFile from "@/components/UploadFile.vue"
 export default {
   name: "Upload",
   components: {
     NavBar,
+    UploadFile,
   },
 };
 </script>
