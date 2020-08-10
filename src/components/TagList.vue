6fcae5684e3157df41f239b93c5b75cc40df5c59
JYJ <1205748010@qq.com>
2020-08-10 16:51:57 +0800

    dev

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="TagList">
-        <div class="tag1">
-            
+        <div class="tag">
+            <rect width="30" height="10" rx="20" ry="20" style="fill:yellow;stroke-width:3;stroke:red"/>
         </div>
     </div>
 </template>
--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="SearchResult">
-        
+        <taglist></taglist>
     </div>
 </template>
+
+<script>
+import taglist from "@/components/TagList.vue";
+export default {
+    name:"SearchResult",
+    components: {
+        taglist,
+    }
+}
+</script>
