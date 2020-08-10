be8a2a473e90be5844d4d68c91b8ae7fcf30b4af
JYJ <1205748010@qq.com>
2020-08-10 17:21:01 +0800

    dev

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -1,7 +1,62 @@
 <template>
     <div class="TagList">
         <div class="tag">
-            <rect width="30" height="10" rx="20" ry="20" style="fill:yellow;stroke-width:3;stroke:red"/>
+            
+            <div class="rectangle1"></div>
+            <span class="TagWords">橘猫</span>
+        </div>
+        <div class="tag">
+            <div class="rectangle2"></div>
+            <span class="TagWords">橘猫</span>
+        </div>
+        <div class="tag">
+            <div class="rectangle3"></div>
+            <span class="TagWords">橘猫</span>
         </div>
     </div>
 </template>
+
+<style>
+.TagList
+{
+    float: left;
+    font-size: 30px;
+}
+.tag
+{
+    margin-top:20px;
+    border:0.5px solid grey;
+    border-spacing: 10px 10px ;
+    border-radius: 15px;
+    
+}
+
+.rectangle1
+{
+    margin-top:10px;
+    float: left;
+    width:30px;
+    height:30px;
+    background:blue;
+}
+.rectangle2
+{
+    margin-top:10px;
+    float: left;
+    width:30px;
+    height:30px;
+    background:orange;
+}
+.rectangle3
+{
+    margin-top:10px;
+    float: left;
+    width:30px;
+    height:30px;
+    background:green;
+}
+.TagWords1
+{
+    float: left;
+}
+</style>
