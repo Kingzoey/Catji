c2c7ea9ba6449f78a2883096cbfe78ace5e42e74
Kingzoey <729650614@qq.com>
2020-08-11 01:09:46 +0800

    Add NavBar to videolist.vue

--- src/views/videolist.vue
+++ src/views/videolist.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="videolist">
+        <NavBar2></NavBar2>
         <videolist></videolist>
     </div>
 </template>
 
 <script>
 import videolist from "@/components/VideoList.vue";
+import NavBar2 from "@/components/NavBar.vue";
 export default {
     name:"SearchResult",
     components: {
         videolist,
+        NavBar2,
     }
 }
 </script>
