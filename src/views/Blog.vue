75d2e4903917e90d22fad6adb2ecf8425a5cb8c3
Frezcirno <2277861660@qq.com>
2020-08-13 20:11:11 +0800

    动态页动态卡片组件 50%

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <NavBar></NavBar>
+    <NavBar />
+    <BlogCard />
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar_Home.vue";
+import BlogCard from "@/components/BlogCard.vue";
+import NavBar from "@/components/NavBar.vue";
 export default {
   name: "Blog",
   components: {
     NavBar,
+    BlogCard,
   },
 };
 </script>
