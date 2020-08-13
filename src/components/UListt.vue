dd01918530715fb2e4bc10bdd9bde8c3819e598d
Kingzoey <729650614@qq.com>
2020-08-13 13:11:49 +0800

    Add UserList.vue and UList.vue.

---
+++ src/components/UListt.vue
@@ -0,0 +1,289 @@
+<template>
+  <div data-v-420e12aa data-v-c25fd0ba class="live-panel">
+    <p data-v-420e12aa class="title">
+      <span data-v-420e12aa class="tc-black ls-0">正在直播</span>
+      <span data-v-420e12aa class="count fs-14 ls-0">（10）</span>
+    </p>
+    <div data-v-420e12aa class="more-button c-pointer">
+      <a
+        data-v-420e12aa
+        href="//link.bilibili.com/p/center/index#/user-center/follow/1"
+        target="_blank"
+        class="more tc-slate"
+      >更多</a>
+    </div>
+    <div data-v-420e12aa class="live-up-list">
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/3"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i0.hdslb.com/bfs/face/5d35da6e93fbfb1a77ad6d1f1004b08413913f9a.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/3"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">3号直播间</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">哔哩哔哩音悦台</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/80476"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i2.hdslb.com/bfs/face/2ffb44cfda7755aaf9a34ca916e2002948b6e1be.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/80476"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">此生不灭夜</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">崩3萌新晨肝</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/5887574"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i2.hdslb.com/bfs/face/4b6a1a73866a70dcac5cd4a0df4cd8d8d19f8d1d.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/5887574"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div
+            data-v-2284ddc9
+            class="up-name line-clamp-1"
+            data-userinfo-popup-inited="true"
+          >网易第五人格手游</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">哔哩哔哩秃秃杯</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/155"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i2.hdslb.com/bfs/face/bf6913644baa030d5f8bc25a91eaeaea418c982e.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/155"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">奈さま</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">暴躁糖豆人！</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/749359"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i1.hdslb.com/bfs/face/3dd277592ff19eb7b1137f364fb22d3482c0126f.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/749359"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div
+            data-v-2284ddc9
+            class="up-name line-clamp-1"
+            data-userinfo-popup-inited="true"
+          >灵魂语者ouo</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">时空中的绘旅人乙女游戏测试直播间</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/244"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i2.hdslb.com/bfs/face/508a9a3e28fd717d20e13ac71443a2eec8d5ab3f.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/244"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">铃椛</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">抵抗虫潮【银河破裂者】</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/10729306"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i1.hdslb.com/bfs/face/2f97dea3da1ed5766851edf84f931a05780eb125.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/10729306"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">会飞的芽子</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">圣人惠COS（封面仅供参考</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/14694782"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i2.hdslb.com/bfs/face/d653a245999ed2096a0ab0ab36f504d994b2f439.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/14694782"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">是袋鼠妹妹</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">Gkd进来看你的婆娘</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/695"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i0.hdslb.com/bfs/face/d36910f231da0483ec28e7616dbb9762337c5121.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/695"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">昭羽君吖</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">一臭打游戏的</div>
+        </a>
+      </div>
+      <div
+        data-v-2284ddc9
+        data-v-420e12aa
+        class="live-panel-item live-up"
+        style="margin-bottom: 20px;"
+      >
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/9922197"
+          target="_blank"
+          class="live-up-img"
+          style="background-image: url(&quot;//i2.hdslb.com/bfs/face/17f1f3994cb4b2bba97f1557ffc7eb34a05e119b.jpg@50w_50h.webp&quot;);"
+          data-userinfo-popup-inited="true"
+        ></a>
+        <a
+          data-v-2284ddc9
+          href="https://live.bilibili.com/9922197"
+          target="_blank"
+          class="live-detail ls-0"
+        >
+          <div data-v-2284ddc9 class="up-name line-clamp-1" data-userinfo-popup-inited="true">丸嘻嘻</div>
+          <div data-v-2284ddc9 class="live-name line-clamp-2">保证和封面一样</div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+body {
+	font-family: Helvetica Neue, Helvetica, Arial, Microsoft Yahei, Hiragino Sans GB, Heiti SC, WenQuanYi Micro Hei, sans-serif;
+	color: #222;
+	font-size: 14px;
+}
+.adaptive-scroll[data-v-08ab5c2b] {
+	-webkit-box-flex: 1;
+	-ms-flex: 1;
+	flex: 1;
+}
+* {
+	margin: 0;
+	padding: 0;
+}
+元素 {
+	min-height: 0px;
+}
+</style>
---
+++ src/views/UList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ulistt">
+  <NavBar2></NavBar2>
+    <UListt style="background-color: #fbfbfb;"></UListt>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar2 from "@/components/NavBar.vue";
+import UListt from "@/components/UListt.vue";
+export default {
+  name: "UList",
+  components: {
+    UListt,
+    NavBar2,
+  },
+};
+</script>
