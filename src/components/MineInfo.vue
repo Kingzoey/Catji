ce26a73c9aaeb8b4935987d4184f40cc07970a54
JYJ <1205748010@qq.com>
2020-08-19 15:25:31 +0800

    增加了更换头像和邮箱，并进行了一些小修改，更换头像的脚本也添加上去了，但不知道为什么不起作用

--- src/components/MineInfo.vue
+++ src/components/MineInfo.vue
@@ -2,6 +2,17 @@
     <div class="MineInfo">
         <form method="post" name="form1">
             <li class="user">
+                <div class="item_bock head_p">
+                    <div class="head_img">
+                        <img :src="test1" width="30" height="30"/>
+                    </div>
+                    <div class="setting_right" @click="uploadHeadImg">
+                        <div class="caption">更改头像</div>
+                    </div>
+                    <input type="file" accept="image/jpg" @change="handleFile(this)" class="hiddenInput"/>
+                </div>
+            </li>
+            <li class="user">
                 <label for="username">昵称：</label>
                 <textarea class="username" placeholder="王二狗子" rows="1" type="textarea" validateevent="true"></textarea>
             </li>
@@ -17,9 +28,14 @@
                 <label for="signal">个性签名：</label>
                 <textarea class="signal" placeholder="设置您的签名" rows="2" autocomplete="off" type="textarea" validateevent="true"></textarea>
             </li>
+            <br>
             <li class="user">
                 <label for="birthday">出生日期：</label>
                 <textarea class="birthday" placeholder="2000-01-01" rows="1" type="textarea" validateevent="true"></textarea>
+            </li>
+            <li class="user emailclass">
+                <label for="email">邮箱：</label>
+                <textarea class="email" placeholder="xxxx@xxx.xxx" rows="1" type="textarea" validateevent="true"></textarea>
             </li>
             <li class="save">
                 <button>保存</button>
@@ -28,7 +44,76 @@
     </div>
 </template>
 
+<script>
+import test1 from "@/assets/test.png";
+export default {
+    name: "Middle",
+    data() {
+        return {
+            test1: test1,
+        };
+    },
+    
+    methods: {
+    // 打开图片上传
+    uploadHeadImg: function () {
+        this.$el.querySelector('.hiddenInput').click()
+    },
+    // 将头像显示
+    handleFile: function (e) {
+        let $target = e.target || e.srcElement
+        let file = $target.files[0]
+        var reader = new FileReader()
+        reader.onload = (data) => {
+            let res = data.target || data.srcElement
+            this.userInfo.avatar = res.result
+        }
+        reader.readAsDataURL(file)
+    },
+  }
+}
+</script>
+
 <style>
+*
+{
+    list-style: none;
+}
+.item_bock {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height:94px;
+  width: 300px;
+  padding:0px 24px 0px 38px;
+  border-bottom: 1px solid #f7f7f7;
+  background: #fff;
+}
+.head_p {
+  height:132px;
+}
+.head_img{
+  height: 90px;
+}
+.head_img img{
+  width:90px;
+  height:90px;
+  border-radius:50px
+}
+.setting_right{
+  display: flex;
+  height: 37px;
+  justify-content: flex-end;
+  align-items: center;
+}
+.hiddenInput{
+  display: none;
+}
+.caption {
+  color: #8F8F8F;
+  font-size: 26px;
+  height: 37px;
+}
 .user label
 {
     width: 90px;
@@ -50,6 +135,11 @@
     width: 618px;
     height: 88px;
     resize: none;
+    vertical-align:text-top;
+}
+.emailclass
+{
+    margin-left: 195px;
 }
 .save button
 {
