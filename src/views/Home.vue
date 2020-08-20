07b7b899a3f1f7d45b4cf4d2867c88dd289b78e9
Ma2Hua <1007543581@qq.com>
2020-08-20 11:33:44 +0800

    修改首页的布局方式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="home">
-	<NavBar></NavBar>
-    <Top></Top>
-	<Top_0></Top_0>
-	<Top_1></Top_1>
-    <Middle></Middle>
-    <Middle2></Middle2>
-    <Middle3></Middle3>
+    <NavBar />
+    <div class="container">
+      <div class="left">
+       <Middle></Middle>
+        <Middle2></Middle2>
+        <Middle3></Middle3> 
+      </div>
+      <div class="right">
+        <Top></Top>
+        <Top_0></Top_0>
+        <Top_1></Top_1>
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script>
 // @ is an alias to /src
@@ -22,18 +29,18 @@ import Middle3 from "@/components/Middle3.vue";
 export default {
   name: "Home",
   components: {
-	NavBar,
+    NavBar,
     Top,
-	Top_0,
-	Top_1,
+    Top_0,
+    Top_1,
     Middle,
     Middle2,
-    Middle3
+    Middle3,
   },
 };
 </script>
 <style scoped>
-	/*
+/*
 	*{margin: 0; padding: 0;}
 	.nav{
 		width: 100%;
@@ -44,4 +51,29 @@ export default {
 		z-index:8;
 	}
 	*/
+.home {
+  
+  width: 100%;
+  background-color: rgb(232, 248, 255);
+}
+.container {
+	margin: auto;
+  display: table;
+  width: 80%;
+  display: flex;
+    
+    margin-bottom: 40px;
+}
+.left,
+.right {
+  
+  display: table-cell;
+}
+.left {
+  width: 1200px;
+
+}
+.right {
+
+}
 </style>
