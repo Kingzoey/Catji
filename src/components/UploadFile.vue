95e16a49e90832f374b3201a8f409c31ae415404
heru-h <2250221147@qq.com>
2020-08-22 20:18:51 +0800

    完善上传文件功能

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -9,21 +9,24 @@
     <div class="file-preview"></div>
     <div class="file-box">
       <div class="upload-icon">
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="upload"
-          class="svg-inline--fa fa-upload fa-w-16"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="currentColor"
-            d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+        <div style="position: relative;">
+          <input
+            id="upload-input"
+            style="position: absolute; top: 0; bottom: 0; left: 0;right: 0; opacity: 0;"
+            type="file"
+            accept="image/gif, image/jpg, image/png"
+            multiple
           />
-        </svg>
+          <!-- 自定义按钮效果 -->
+          <div style="text-align: top">
+            <span style="font-size: 12px;"></span>
+            <img
+              id="upload"
+              src="@/assets/upload.png"
+              style=" vertical-align: middle;"
+            />
+          </div>
+        </div>
       </div>
       <div class="upload-tips">
         <div class="tips-head">
@@ -135,4 +138,23 @@ export default {
 .upload-btn:hover {
   background-color: #ec4556;
 }
+
+/*
+       <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="upload"
+          class="svg-inline--fa fa-upload fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path
+            fill="currentColor"
+            d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+          />
+        </svg>
+
+*/
 </style>
