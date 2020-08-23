54c2eef94e778ad86ca3c6188262354a33664d30
Frezcirno <2277861660@qq.com>
2020-08-23 11:26:55 +0800

    改了一下首页样式, 美观度+1

--- src/components/Middle2.vue
+++ src/components/Middle2.vue
@@ -2,52 +2,46 @@
   <div class="new-part">
     <div class="card">
       <header class="new-header">
-        <br />
         <div class="icon"></div>
         <span class="new-name">
-          <font-awesome-icon :icon="['fas', 'newspaper']" />&nbsp;最 新
+          <slot name="header"></slot>
         </span>
       </header>
-      <div class="new-list-top">
+      <div class="new-list-top clearfix">
         <div class="top-left">
           <div class="big-item">
             <a class="cover" href="#" target="_blank">
               <img :src="test1" height="208" width="370" alt="最新视频1" />
             </a>
-            <br />
             <a title="最新视频1">最新视频1</a>
           </div>
         </div>
         <div class="top-right">
-          <div class="top-right-line1">
+          <div class="top-right-line1 clearfix">
             <div class="normal-item">
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频2" />
               </a>
-              <br />
               <a title="最新视频2">最新视频2</a>
             </div>
             <div class="normal-item">
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频3" />
               </a>
-              <br />
               <a title="最新视频3">最新视频3</a>
             </div>
           </div>
-          <div class="top-right-line2">
+          <div class="top-right-line2 clearfix">
             <div class="normal-item">
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频5" />
               </a>
-              <br />
               <a title="最新视频5">最新视频5</a>
             </div>
             <div class="normal-item">
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频6" />
               </a>
-              <br />
               <a title="最新视频6">最新视频6</a>
             </div>
           </div>
@@ -58,35 +52,29 @@
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频8" />
               </a>
-              <br />
               <a title="最新视频8">最新视频8</a>
             </div>
             <div class="normal-item">
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频9" />
               </a>
-              <br />
               <a title="最新视频9">最新视频9</a>
             </div>
             <div class="normal-item">
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频10" />
               </a>
-              <br />
               <a title="最新视频10">最新视频10</a>
             </div>
             <div class="normal-item">
               <a class="cover" href="#" target="_blank">
                 <img :src="test1" height="90" width="175" alt="最新视频11" />
               </a>
-              <br />
               <a title="最新视频11">最新视频11</a>
             </div>
           </div>
         </div>
       </div>
-      <br />
-      <br />
     </div>
   </div>
 </template>
@@ -106,42 +94,47 @@ export default {
 </script>
 
 <style>
+.new-part{
+  height: 100%;
+}
+.card img {
+  border-radius: 5px;
+  display: block;
+}
 .card {
-  background-color: rgb(255, 255, 255);
-  border-radius: 10px;
+  padding-left: 30px;
+  padding-top: 10px;
 }
 .new-part {
   position: inherit;
 }
-.new-list-top:after {
-  content: "";
-  display: block;
-  clear: both;
-}
+
 .new-list-top {
   white-space: nowrap;
+  margin-top: 10px;
 }
 .new-name {
-  font-size: 30px;
+  font-size: 22px;
+  line-height: 36px;
 }
 .new-header {
-  margin-left: 10px;
+  height: 36px;
 }
 .top-left {
   display: inline-block;
-  margin: -31px;
+  vertical-align: top;
 }
 .bottom {
   float: left;
 }
 .normal-item {
   float: left;
-  width: 12em;
-  border: 200px;
-  margin-left: 50px;
+  width: 195px;
+  height: 118px;
 }
 .top-right {
   display: inline-block;
-  margin-right: 10px;
+  vertical-align: top;
+  margin-left: 20px;
 }
 </style>
