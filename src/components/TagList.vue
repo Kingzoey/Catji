9f36439f537908ab5af9472109fe4051667e2c9a
Ma2Hua <1007543581@qq.com>
2020-08-25 17:02:50 +0800

    添加搜索结果点击tag跳转功能

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -1,71 +1,63 @@
 <template>
-    <div class="TagList">
-        <div class="tag">
-            <div class="rectangle1"></div>
-            <span class="TagWords">“</span>
-            <span class="TagWords">橘猫</span>
-            <span class="TagWords">”</span>
-
-        </div>
-        <br>
-        <div class="tag">
-            <div class="rectangle2"></div>
-            <span class="TagWords">  “</span>
-            <span class="TagWords">哆啦a梦</span>
-            <span class="TagWords">”</span>
-        </div>
-        <br>
-        <div class="tag">
-            <div class="rectangle3"></div>
-            <span class="TagWords">  “</span>
-            <span class="TagWords">橘猫123122131231231233</span>
-            <span class="TagWords">”</span>
-        </div>
+  <div class="TagList">
+    <div class="tag">
+      <div class="rectangle1"></div>
+      <a class="cover" href="#" target="_blank">
+        <span class="TagWords">“橘猫”</span>
+      </a>
     </div>
+    <br />
+    <div class="tag">
+      <div class="rectangle2"></div>
+      <a class="cover" href="#" target="_blank">
+        <span class="TagWords">“哆啦a梦”</span>
+      </a>
+    </div>
+    <br />
+    <div class="tag">
+      <div class="rectangle3"></div>
+      <a class="cover" href="#" target="_blank">
+        <span class="TagWords">“橘猫123122131231231233”</span>
+      </a>
+    </div>
+  </div>
 </template>
 
 <style>
-.TagList
-{
-    float: left;
-    font-size: 20px;
+.TagList {
+  float: left;
+  font-size: 20px;
 }
-.tag
-{
-    margin-top:20px;
-    border:0.5px solid grey;
-    padding:10px 10px;
-    border-radius: 15px;
-    display: inline-block;
-    
+.tag {
+  margin-top: 20px;
+  border: 0.5px solid grey;
+  padding: 10px 10px;
+  border-radius: 15px;
+  display: inline-block;
 }
 
-.rectangle1
-{
-    margin-top:10px;
-    margin-right: 10px;
-    float: left;
-    width:15px;
-    height:15px;
-    background:blue;
+.rectangle1 {
+  margin-top: 10px;
+  margin-right: 10px;
+  float: left;
+  width: 15px;
+  height: 15px;
+  background: blue;
 }
-.rectangle2
-{
-    margin-top:10px;
-    margin-right: 10px;
-    float: left;
-    width:15px;
-    height:15px;
-    background:orange;
+.rectangle2 {
+  margin-top: 10px;
+  margin-right: 10px;
+  float: left;
+  width: 15px;
+  height: 15px;
+  background: orange;
 }
-.rectangle3
-{
-    margin-top:10px;
-    margin-right: 10px;
-    float: left;
-    width:15px;
-    height:15px;
-    background:green;
+.rectangle3 {
+  margin-top: 10px;
+  margin-right: 10px;
+  float: left;
+  width: 15px;
+  height: 15px;
+  background: green;
 }
-
 </style>
