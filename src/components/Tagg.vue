1e41a4aefaf2f84593ac22fc3ebc57ce24a7bbe9
Kingzoey <729650614@qq.com>
2020-08-26 17:44:54 +0800

    Yogurt ice-cream style is coming to tag.

--- src/components/Tagg.vue
+++ src/components/Tagg.vue
@@ -71,35 +71,117 @@
       </div>
     </div>
     <!--话题头部主导航-->
-    <div class="m-main-nav">
-      <li>
-        <a class="cur" href="/" title="视频">视频</a>
-      </li>
-      <li>
-        <a href="/" title="动态">动态</a>
-      </li>
-      <li>
-        <a href="/" title="猫咪">猫咪</a>
-      </li>
-      <li>
-        <a href="/" title="Tag">Tag</a>
-      </li>
+    <div class="container">
+      <ul class="tab clearfix">
+        <li
+          class="tab-item"
+          :class="{on:index==on}"
+          v-for="(tab, index) in tablist"
+          :key="tab.name"
+        >
+          <a href="javascript:void(0);" @click="on = index">
+            <font-awesome-icon :icon="['fas', tab.iconname]" />
+            {{tab.name}}
+          </a>
+        </li>
+      </ul>
+      <component :is="tablist[on].tab"></component>
     </div>
-    <!--/话题头部主导航-->
   </div>
 </template>
 
 <script>
+export default {
+  name: "Space",
+  components: {},
+  methods: {},
+  data() {
+    return {
+      on: 0,
+      tablist: [
+        {
+          name: "视频",
+          iconname: "child",
+          tab: () => import("@/components/VideoListtSearch.vue"),
+        },
+        {
+          name: "动态",
+          iconname: "heart",
+          tab: () => import("@/components/BlogCardSearch.vue"),
+        },
+      ],
+    };
+  },
+  mounted() {
+    console.log(this.$route.params.usid);
+  },
+};
 </script>
 
 <style scoped>
-/* card wrap */
-.card-wrap {
-  margin-bottom: 10px;
-  border-radius: 2px;
-  border: 1px solid #eee;
-  background: #fff;
+
+* {
+  margin: 5;
+  padding: 0;
+  list-style: none;
 }
+
+.container {
+  padding: 50 50 50 50px;
+  margin: left 500px;
+}
+
+.tab :hover {
+  color: #5dceea;
+}
+
+.tab {
+  font-weight: normal;
+  font-size: 18px;
+  font-family: Michroma, "Segoe UI Light", "Segoe UI", "Segoe UI WP",
+    "Microsoft Jhenghei", "微软雅黑", sans-serif, Times;
+  border-bottom: 1px solid rgb(232, 248, 255);
+}
+
+.tab-item {
+  margin: 16px auto;
+  padding: 0 0 0 448px;
+  overflow: visible;
+  display: block;
+  float: left;
+}
+
+.tab-item.on a {
+  color: pink;
+}
+
+.tab-item a {
+  overflow: visible;
+  padding: 0 8px;
+  border: none;
+  font-weight: normal;
+  font-size: 18px;
+
+  height: 36px;
+  margin-right: 4px;
+  color: #999;
+}
+
+.tab-item a svg {
+  color: #3a9bd9;
+}
+
+.tab-item.on a svg {
+  color: pink;
+}
+
+.card {
+  margin-left: 200px;
+  margin-right: 200px;
+}
+
+/*修改*/
+
 .m-main-nav
 {
   text-align: center;
@@ -107,31 +189,6 @@
 }
 /* card */
 
-/* card head */
-.card-head {
-  font-size: 14px;
-  font-weight: normal;
-  height: 38px;
-  line-height: 38px;
-  border-bottom: 1px solid #eee;
-  padding: 0 12px 0 11px;
-}
-.card-head .title {
-  font-weight: 700;
-}
-.card-head .more {
-  font-size: 12px;
-}
-/* card top */
-.card-top {
-  height: 30px;
-  line-height: 30px;
-  border-bottom: 1px solid #eee;
-  padding: 0 12px 0 11px;
-}
-.card-top .title {
-  font-weight: normal;
-}
 /* 璇濋 */
 .card-topic {
   background: #fff;
@@ -168,8 +225,8 @@
   opacity: 0;
 }
 .card-topic-a {
-  padding: 26px 20px;
-  border-bottom: 1px solid #e6e6e6;
+  padding: 26px 200px;
+  border-bottom: 1px solid rgb(232, 248, 255);
   position: relative;
 }
 .card-topic-a .spic,
@@ -279,22 +336,14 @@
 .card-topic-b .card-topic-a .total {
   color: #fff;
 }
-/* 璇濋瀵艰 */
+/* card-topic */
 .card-topic-lead {
   padding: 16px;
   font-size: 14px;
-  border-bottom: 1px solid #ebebeb;
+  border-bottom: 1px solid rgb(232, 248, 255);
 }
 
 /* 全局通用样式 */
-/*html, body, div, ul, ol, li, dl, dt, dd, header, footer, hgroup, h1, h2, h3, h4, h5, h6, p, br, section, article, figure, figcaption, sup, img, a, span, i, b, small, em, strong, form, fieldset, legend, label, input, select, option, textarea, button, cite, menu, table, thead, tbody, tfoot, tr, th, td {
-	margin: 0;
-	padding: 0;
-	font-weight: normal;
-	font-size: 100%;
-	font-style: normal;	
-	line-height: 100%;
-}*/
 html,
 body,
 div,
@@ -333,9 +382,6 @@ fieldset,
 legend,
 label,
 input,
-select,
-option,
-textarea,
 button,
 cite,
 menu,
@@ -383,90 +429,14 @@ li {
 button,
 img,
 fieldset,
-input,
-textarea,
-select {
-  border: 0;
-  outline: none;
-}
-textarea {
-  resize: none;
-  font: 12px/1.125 Arial, Helvetica, sans-serif;
-}
-input[type="text"],
-input[type="password"],
-textarea {
-  outline-style: none;
-  -webkit-appearance: none;
-}
 
-/*----style----*/
-/* 自定义字体图标 */
-/*
-@font-face {
-  font-family: "wbicon";
-  src: url("font/wbficon.eot?v=20181128") format("embedded-opentype"),
-    url("font/wbficon.svg?v=20181128") format("svg"),
-    url("font/wbficon.woff?v=20181128") format("woff"),
-    url("font/wbficon.ttf?v=20181128") format("truetype");
-  src: url("font/wbficon.eot?v=20181128") \9;
-  font-weight: normal;
-  font-style: normal;
-}
-*/
-.wbicon {
-  font-family: "wbicon" !important;
-  display: inline-block;
-  -webkit-font-smoothing: antialiased;
-}
-/* 水平线 */
-.s-hr {
-  height: 1px;
-  background: #ddd;
-}
 /* button */
-.s-btn-a,
-.s-btn-b,
-.s-btn-c,
-.s-btn-d,
-.s-btn-e,
-.s-btn-f,
-.s-btn-g,
-.s-btn-h {
+.s-btn-c {
   display: inline-block;
   text-align: center;
   white-space: nowrap;
   border-radius: 2px;
-  padding: 0 10px;
-}
-.s-btn-a {
-  font-size: 12px;
-  min-width: 40px;
-  height: 26px;
-  line-height: 26px;
-  background: #f85d00;
-  color: #fff;
-}
-.s-btn-a:hover {
-  color: #fff;
-  background: #cc560e;
-}
-.s-btn-a.disable {
-  background: #ffc09f;
-  color: #fff;
-  cursor: default;
-}
-.s-btn-b {
-  font-size: 16px;
-  min-width: 100px;
-  height: 36px;
-  line-height: 36px;
-  background: #f85d00;
-  color: #fefefe;
-}
-.s-btn-b:hover {
-  color: #fff;
-  background: #cc560e;
+  padding: 0;
 }
 .s-btn-c {
   font-size: 12px;
@@ -476,147 +446,8 @@ textarea {
   border: 1px solid #d9d9d9;
   background: #fff;
 }
-.s-btn-d {
-  font-size: 12px;
-  min-width: 30px;
-  height: 22px;
-  line-height: 22px;
-  background: #ff8140;
-  color: #fff;
-}
-.s-btn-d:hover {
-  color: #fff;
-  background: #cc560e;
-}
-.s-btn-e {
-  min-width: 40px;
-  height: 20px;
-  line-height: 20px;
-  padding: 0 5px;
-  border: 1px solid #d9d9d9;
-  background: #fff;
-}
-.s-btn-f {
-  font-size: 14px;
-  min-width: 80px;
-  height: 38px;
-  line-height: 38px;
-  background: #09f;
-  color: #fff;
-}
-.s-btn-f:hover {
-  background: #09d;
-  color: #fff;
-}
-.s-btn-g {
-  font-size: 14px;
-  min-width: 60px;
-  height: 30px;
-  line-height: 30px;
-  background: #f85d00;
-  color: #fff;
-}
-.s-btn-g:hover {
-  color: #fff;
-  background: #cc560e;
-}
-.s-btn-g.disable {
-  background: #ffc09f;
-  color: #fff;
-  cursor: default;
-}
-.s-btn-h {
-  font-size: 14px;
-  min-width: 80px;
-  height: 38px;
-  line-height: 38px;
-  background: #fff;
-  border: 1px solid #d9d9d9;
-  box-shadow: 0 1px 1px #ececec;
-}
-.s-btn-h:hover {
-  color: inherit;
-  border: 1px solid #ccc;
-}
-/* tag */
-.s-tag-a {
-  padding: 4px 6px 3px 6px;
-  background: #fff;
-  color: #999;
-  border: 1px solid #ddd;
-  font-weight: 100;
-  font-size: 12px;
-}
-/* 形状 */
-.s-arrow-t {
-  width: 0;
-  height: 0;
-  border-left: 10px solid transparent;
-  border-right: 10px solid transparent;
-  border-bottom: 12px solid #f2f2f5;
-}
-.s-drop-arrow,
-.s-rise-arrow {
-  display: inline-block;
-  width: 14px;
-  height: 14px;
-  margin-left: -8px;
-}
-.s-drop-arrow em,
-.s-rise-arrow em {
-  display: block;
-  width: 5px;
-  height: 7px;
-  margin: auto;
-}
-.s-drop-arrow em {
-  background: #4ab666;
-}
-.s-rise-arrow em {
-  background: #e05a5a;
-}
-.s-drop-arrow i,
-.s-rise-arrow i {
-  display: block;
-  border-width: 7px;
-  border-style: solid dashed dashed dashed;
-  width: 0;
-}
-.s-drop-arrow i {
-  border-color: #4ab666 transparent transparent transparent;
-}
-.s-rise-arrow i {
-  border-color: #e05a5a transparent transparent transparent;
-}
-/* 涓诲鑸� */
-.m-main-nav {
-  background: #fff;
-}
-.m-main-nav ul {
-  width: 810px;
-  margin: auto;
-  height: 40px;
-}
-.m-main-nav li {
-  display: inline-block;
-  margin: 0 5.1%;
-}
-.m-main-nav li a {
-  display: block;
-  padding: 11px 10px 10px;
-  font-size: 14px;
-  color: #333;
-}
-.m-main-nav a:hover {
-  color: #333;
-  font-weight: bolder;
-  border-bottom: 2px solid #f7691c;
-}
-.m-main-nav a.cur {
-  border-bottom: 2px solid #f7691c;
-  font-weight: bolder;
-}
-.wbs-intresting .m-main-nav li {
-  margin: 0 3.7%;
+
+.tagg {
+  background-color: rgb(232, 248, 255);;
 }
 </style>
