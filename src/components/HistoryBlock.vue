68deaf75bfda44a4363883cfdfc88e9a23c71724
heru-h <2250221147@qq.com>
2020-09-08 16:46:35 +0800

    historyblock get data

--- src/components/HistoryBlock.vue
+++ src/components/HistoryBlock.vue
@@ -6,7 +6,7 @@
     <ul>
       <li class="vi-item" v-for="vi in video" :key="vi.vid">
         <div class="v-cover">
-          <router-link :to="/video/ + vi.vid" :title="vi.vtitle" class="coverimg">
+          <router-link :to="/video/ + vi.vid" :title="vi.title" class="coverimg">
             <div class="lazy-img">
               <img :src="vi.cover" />
             </div>
@@ -14,15 +14,15 @@
         </div>
         <div class="info-wrap">
           <div class="headline">
-            <router-link :to="/video/ + vi.vid" :title="vi.vtitle" class="title">{{vi.vtitle}}</router-link>
+            <router-link :to="/video/ + vi.vid" :title="vi.title" class="title">{{vi.title}}</router-link>
           </div>
           <div class="desc">
             <a class="visit-up-space" href="https://space.bilibili.com/168598">
-            <font-awesome-icon :icon="['fas', 'user']" />
-            {{vi.upname}}
+              <font-awesome-icon :icon="['fas', 'user']" />
+              {{vi.nickname}}
             </a>
             <a class="attention-btn" @click="follow(vi.vid)">+ 关注</a>
-            
+
             <a class="attention-a" @click="del(vi.vid)">删除历史</a>
           </div>
         </div>
@@ -32,6 +32,7 @@
 </template>
 
 <script>
+import { watchHistory } from "../api";
 //import  { watchHistory } from "../api";
 export default {
   name: "HistoryBlock",
@@ -40,56 +41,76 @@ export default {
       video: [
         {
           vid: 1,
-          vtitle: "小猫咪叫人起床",
-          upname: "王五锤", //上传视频up主
+          title: "小猫咪叫人起床",
+          nickname: "王五锤", //上传视频up主
           cover:
             "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2418940404,2594675617&fm=26&gp=0.jpg", //封面
         },
         {
           vid: 2,
-          vtitle: "可爱猫咪日常搞笑合集",
-          upname: "王大锤", //上传视频up主
+          title: "可爱猫咪日常搞笑合集",
+          nickname: "王大锤", //上传视频up主
           cover:
             "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2338360597,1299861243&fm=26&gp=0.jpg", //封面
         },
         {
           vid: 3,
-          vtitle: "这么可爱的猫咪你下得去手",
-          upname: "王二锤", //上传视频up主
+          title: "这么可爱的猫咪你下得去手",
+          nickname: "王二锤", //上传视频up主
           cover:
             "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3294506463,533319956&fm=26&gp=0.jpg", //封面
         },
         {
           vid: 4,
-          vtitle: "云吸猫",
-          upname: "王三锤", //上传视频up主
+          title: "云吸猫",
+          nickname: "王三锤", //上传视频up主
           cover:
             "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=338026201,811635179&fm=26&gp=0.jpg", //封面
         },
         {
           vid: 5,
-          vtitle: "同济人气猫咪",
-          upname: "王四锤", //上传视频up主
+          title: "同济人气猫咪",
+          nickname: "王四锤", //上传视频up主
           cover:
             "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=187649172,1956357065&fm=26&gp=0.jpg", //封面
         },
-        
       ],
     };
   },
   methods: {
     follow() {
       window.alert("关注成功!(狗头)");
-    },del() {
+    },
+    del() {
       window.alert("已删除!");
     },
+  },
+
+  beforeMount() {
+    watchHistory() // 函数调用返回的是Promise
+      .then((res) => {
+        res = res.data;
+        console.log(res);
+        if (res.status === "ok") {
+          this.video = res.data; // 请求成功后, this.video会被设置为res.data的内容, 从而触发页面更新
+          console.log(res);
+       
+        } else {
+          // ...
+          console.log("请求错误，错误信息 :" + res.status);
+        }
+      })
+      .catch((err) => {
+        console.log("网络失败");
+        console.log(err);
+      }); // 这里和1.1一样写处理函数
   },
 };
 </script>
 
 <style scoped>
 .title-his {
-  color:#00a1d6;
+  color: #00a1d6;
   font-size: 20px;
   padding-left: 5px;
   padding-top: 2px;
@@ -151,7 +172,7 @@ export default {
 }
 
 .visit-up-space a:hover {
-  color:pink;
+  color: pink;
 }
 
 .desc :hover {
@@ -170,7 +191,7 @@ export default {
   display: inline-block;
   vertical-align: middle;
 }
-.attention-a{
+.attention-a {
   width: 80px;
   height: 24px;
   text-align: center;
@@ -184,6 +205,6 @@ export default {
   float: right;
 }
 .headline :hover {
-  color:#00a1d6;
+  color: #00a1d6;
 }
 </style>
