fc7889cf82dee8b0bde2afc84d6a39f52a6d16a1
Kingzoey <729650614@qq.com>
2020-09-15 13:32:21 +0800

    Fix the background of Seach Page.

--- src/components/Registerr.vue
+++ src/components/Registerr.vue
@@ -28,7 +28,6 @@
         <div class="send-button">
           <input type="button" value="注册" @click="onRegister()" />
         </div>
-        
       </div>
     </div>
   </div>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -128,7 +128,7 @@ input {
   position: absolute;
   top: 0;
   background: url("../assets/test3.png") repeat;
-  height: 110%;
+  height: 131%;
   width: 100%;
   filter: blur(2px);
   background-size: cover;
