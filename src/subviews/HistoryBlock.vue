3ae75fedf7ce97eb38463a968460b82b78d89762
Frezcirno <2277861660@qq.com>
2020-09-15 23:07:59 +0800

    观看历史页接口更新

--- src/subviews/HistoryBlock.vue
+++ src/subviews/HistoryBlock.vue
@@ -9,25 +9,33 @@
       </div>
     </div>
     <ul>
-      <li class="vi-item" v-for="vi in dataList" :key="vi.vid">
+      <li class="vi-item" v-for="(vi, index) in dataList" :key="vi.video.vid">
         <div class="v-cover">
-          <router-link :to="/video/ + vi.vid" :title="vi.title" class="coverimg">
+          <router-link :to="/video/ + vi.video.vid" :title="vi.video.title" class="coverimg">
             <div class="lazy-img">
-              <img :src="vi.cover" />
+              <img :src="vi.video.cover" />
             </div>
           </router-link>
         </div>
         <div class="info-wrap">
           <div class="headline">
-            <router-link :to="/video/ + vi.vid" :title="vi.title" class="title">{{vi.title}}</router-link>
+            <router-link
+              :to="/video/ + vi.video.vid"
+              :title="vi.video.title"
+              class="title"
+            >{{vi.video.title}}</router-link>
           </div>
           <div class="desc">
-            <router-link class="visit-up-space" :to="/video/ + vi.vid">
+            <router-link class="visit-up-space" :to="/space/ + vi.video.up.usid">
               <font-awesome-icon :icon="['fas', 'user']" />
-              {{vi.nickname}}
+              {{vi.video.up.name}}
             </router-link>
-            <button class="attention-btn" @click="follow(index)">+ 关注</button>
-            <button class="attention-a" @click="del(index)">删除历史</button>
+            <el-button
+              :type="vi.video.up.ifollow?'success':'primary'"
+              size="mini"
+              @click="onFollow(index)"
+            >{{vi.video.up.ifollow?"已关注":"关注"}}</el-button>
+            <el-button type="danger" size="mini" @click="del(index)">删除历史</el-button>
           </div>
         </div>
       </li>
@@ -51,17 +59,38 @@ export default {
     };
   },
   methods: {
-    follow(index) {
+    onFollow(index) {
       if (!this.$store.state.user.usid) {
         this.$message.error("登录后才能关注up主");
         return;
       }
-      index;
-      follow;
-      unfollow; // 关注不了
+
+      let up = this.dataList[index].video.up;
+      if (up.ifollow) {
+        unfollow(up.usid)
+          .then(() => {
+            up.ifollow = up.ifollow ? 0 : 1;
+          })
+          .catch((err) => {
+            if (err.response.data.status === "未关注此人") {
+              up.ifollow = 0;
+            }
+          });
+      } else {
+        follow(up.usid)
+          .then(() => {
+            up.ifollow = up.ifollow ? 0 : 1;
+          })
+          .catch((err) => {
+            if (err.response.data.status === "已经关注此人") {
+              up.ifollow = 1;
+            }
+          });
+      }
     },
     del(index) {
-      index; // 目前不支持删除观看历史
+      this.dataList.splice(index, 1);
+      // 目前不支持删除观看历史
     },
     getData(page) {
       watchHistory(this.$props.usid, page) // 函数调用返回的是Promise
@@ -149,14 +178,6 @@ export default {
   line-height: 16px;
 }
 
-.visit-up-space a:hover {
-  color: pink;
-}
-
-.desc :hover {
-  color: pink;
-}
-
 .attention-btn {
   width: 65px;
   height: 24px;
@@ -180,8 +201,9 @@ export default {
   cursor: pointer;
   display: inline-block;
   vertical-align: middle;
-  float: right;
 }
+
+.visit-up-space:hover,
 .headline :hover {
   color: #00a1d6;
 }
